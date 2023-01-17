09eab114e5f69f6e34a3c264bcd197f4e1b29149
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-01-16 23:38:57 -0300

    feat: logon screen

--- src/components/loginContainer/loginContainer.component.tsx
+++ src/components/loginContainer/loginContainer.component.tsx
@@ -5,64 +5,53 @@ import * as S from "./loginContainer.styles";
 export function LoginContainer() {
   return (
     <S.Container>
-      <mui.Container maxWidth="xs">
+      <S.Form>
+        <mui.Typography component="h1" variant="h5">
+          Entrar
+        </mui.Typography>
         <mui.Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            backgroundColor: "#ffffff",
-            padding: 4,
-            borderRadius: 2,
-          }}
+          component="form"
+          onSubmit={() => console.log("arroz")}
+          noValidate
+          sx={{ mt: 1 }}
         >
-          <mui.Typography component="h1" variant="h5">
-            Entrar
-          </mui.Typography>
-          <mui.Box
-            component="form"
-            onSubmit={() => console.log("arroz")}
-            noValidate
-            sx={{ mt: 1 }}
+          <mui.TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <mui.TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <mui.Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, backgroundColor: "#1635ff" }}
           >
-            <mui.TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <mui.TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <mui.Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Entrar
-            </mui.Button>
-            <mui.Grid container>
-              <mui.Grid item xs>
-                <mui.Link href="#" variant="body2">
-                  Esqueceu a senha?
-                </mui.Link>
-              </mui.Grid>
+            Entrar
+          </mui.Button>
+          <mui.Grid container>
+            <mui.Grid item xs>
+              <mui.Link href="#" color="#1635ff" variant="body2">
+                Esqueceu a senha?
+              </mui.Link>
             </mui.Grid>
-          </mui.Box>
+          </mui.Grid>
         </mui.Box>
-      </mui.Container>
+      </S.Form>
     </S.Container>
   );
 }
--- src/components/loginContainer/loginContainer.styles.ts
+++ src/components/loginContainer/loginContainer.styles.ts
@@ -1,11 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-image: url("login.jpg");
+  background-color: #1b1b1b;
   width: 100%;
   height: 100vh;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
+  position: relative;
+`;
+export const Form = styled.div`
+  background-color: #f2f2f2;
+  padding: 32px;
+  width: 33%;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  border-radius: 8px;
 `;
