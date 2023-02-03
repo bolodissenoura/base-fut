31ccb861dbfb386b1a9bef8d7ec8238974a5a809
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-02-02 21:50:00 -0300

    chore: install tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="">sijausahu</div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </main>
     </>
   );
