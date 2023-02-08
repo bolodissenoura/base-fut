d4237573dc1cfbc68f1e15d98cfb6b6f42bf7e78
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-02-08 07:51:25 -0300

    chore: undo context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import type { AppProps } from "next/app";
-import { AuthProvider } from "../context/authContext"
 import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Component {...pageProps} />
     </>
   );
 }
