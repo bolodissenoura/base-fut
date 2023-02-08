7dea1de7ad0b3e40db93c0ee84e0cf54babc5023
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-02-07 23:46:11 -0300

    feat: add context to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from "next/app";
+import { AuthProvider } from "../context/authContext"
 import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </>
   );
 }
