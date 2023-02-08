b8cca88fd76da26a68110dc907bc2176684a781d
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-02-08 08:33:46 -0300

    fix: warning img

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -23,14 +23,13 @@ export default function Login() {
         <section className="flex flex-col md:flex-row h-screen items-center">
 
           <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-            <Image src="/loginWpp.jpg" alt="aa" width={10000} height={10000} className="w-full h-full object-cover" />
+            <Image src="/loginWpp.jpg" priority alt="aa" width={10000} height={10000} className="w-full h-full object-cover" />
           </div>
 
           <div className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
       flex items-center justify-center">
 
             <div className="w-full h-100">
-
               <div className="w-100 h-1 flex justify-center items-center">
                 <Image src="/logo.png" alt="aa" width={200} height={30} className="object-cover" />
               </div>
